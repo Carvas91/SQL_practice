502f08e44e8480e88de1c9a17816245b68a8bc97
carvas91 <carvas91@hotmail.com>
2024-12-24 15:29:40 -0500

    comparisson operators and column name update

--- Netflix/Netflix_queries.sql
+++ Netflix/Netflix_queries.sql
@@ -2,7 +2,7 @@ SELECT episodio_id, descripcion, titulo from Episodios;
 
 SELECT DISTINCT genero from Series;
 
-SELECT DISTINCT año_lanzamiento from Series ORDER by año_lanzamiento asc;
+SELECT DISTINCT anio_lanzamiento from Series ORDER by anio_lanzamiento asc;
 
 SELECT titulo, duracion FROM Episodios ORDER by duracion desc;
 
@@ -13,3 +13,5 @@ SELECT titulo, duracion from Episodios ORDER by duracion ASC LIMIT 10;
 SELECT DISTINCT genero FROM series;
 
 SELECT *  FROM Series WHERE genero = 'Drama';
+
+SELECT * from Series WHERE anio_lanzamiento > 2015 ORDER by anio_lanzamiento ASC;
