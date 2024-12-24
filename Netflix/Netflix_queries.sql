4f939dfa3e90569dd2f1d483b3765c49a707701a
carvas91 <carvas91@hotmail.com>
2024-12-23 21:47:06 -0500

    where and limit

--- Netflix/Netflix_queries.sql
+++ Netflix/Netflix_queries.sql
@@ -3,3 +3,13 @@ SELECT episodio_id, descripcion, titulo from Episodios;
 SELECT DISTINCT genero from Series;
 
 SELECT DISTINCT año_lanzamiento from Series ORDER by año_lanzamiento asc;
+
+SELECT titulo, duracion FROM Episodios ORDER by duracion desc;
+
+SELECT * from Episodios LIMIT 10;
+
+SELECT titulo, duracion from Episodios ORDER by duracion ASC LIMIT 10;
+
+SELECT DISTINCT genero FROM series;
+
+SELECT *  FROM Series WHERE genero = 'Drama';
